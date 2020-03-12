32e0935f02d86fd7884f14de1926dfc24fbacc6a
joharbatta <joharbatta@gmail.com>
2020-03-13 01:17:12 +0530

    input field

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React,{Component} from 'react';
+
+class App extends Component {
+  render()
+  {
+
+    return (
+      <div className="App">
+        <div className="title">
+        Reminder Pro
+        </div>
+        <div className="form-inline">
+        <div className="from-group">
+        <input
+          className="form-control"
+          placeholder="MISSION>"
+          />
+        </div>
+        <button
+          type="button"
+          className="btn btn-success"
+          >Add Reminder</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
